016f8727140da781647140ba1c749597efd41e6a
Ajdin <ajdinseho@hotmail.com>
2025-04-29 01:37:45 -0400

    Initial Commit

---
+++ GymApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GymApp
+//
+//  Created by Ajdin Seho on 4/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GymAppTests/GymAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GymAppTests.swift
+//  GymAppTests
+//
+//  Created by Ajdin Seho on 4/29/25.
+//
+
+import Testing
+@testable import GymApp
+
+struct GymAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
